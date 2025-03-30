9317e364c396b2a444ba08df3c1b229d0ed54619
Timilehin Daramola <timi.homezone111@gmail.com>
2025-03-29 20:43:03 -0400

    routes updated

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,13 @@ router.get('/', (req, res) => {
   //swagger.tags = ['Hello world'];
     res.render('./products')
   })
+
+  router.get('/', (req, res) => {
+    //swagger.tags = ['Hello world'];
+      res.render('./customers')
+    })
   
 router.use('/products', require('./products'));
+router.use('/customers', require('./customers'));
 
 module.exports = router;
