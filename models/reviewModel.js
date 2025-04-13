6036b2d084342217bd70f4c93a7677dd4facbfca
Timilehin Daramola <timi.homezone111@gmail.com>
2025-04-13 18:42:55 -0400

    cse341

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -8,14 +8,15 @@ const getReviewCollection = () => {
 // Add a review for a product
 const addReview = async (review) => {
     const collection = getReviewCollection();
+    review.name = review.name.toLowerCase(); // Normalize the name to lowercase
     const result = await collection.insertOne(review);
-    return { ...review, _id: result.insertedId }; // Return the newly created review with its ID
+    return { ...review, _id: result.insertedId };
 };
 
 // Retrieve reviews for a specific product by name
 const getReviewsByProductName = async (name) => {
     const collection = getReviewCollection();
-        return await collection.find({ name }).toArray();
+    return await collection.find({ name: name.toLowerCase() }).toArray();
 };
 
 module.exports = {
